eec31bb09107670e318728dc3c27c3613ce59b1a
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-09-08 17:36:28 -0700

    fourth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - My first c code
- *
+ * main - my first entry
+ * 
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle"))
 	return (0);
 }
