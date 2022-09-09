1d046f255d52d2fbadef2dd7db87fbd96b9bb017
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-09-09 16:50:38 -0700

    first if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,21 +2,31 @@
 #include <time.h>
 #include <stdio.h>
 
-/* main is about to know if code is right */
-
-/* to ready the code we will use betty */
+/**
+* main - print variable n
+*
+* Description: using the main function
+* n will be positive, zero, or negative
+* Return: 0
+*/
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-    if (n > 0) 
-    printf("%i is postive\n",n);
-    else if (n == 0) 
-    printf("%i is zero\n",n);
-    else (n < 0);
-    printf("%i is negative\n",n);
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+  /* your code goes there */
+if (n > 0)
+{
+	printf("%i is positive\n", n);
+}
+else if (n == 0)
+{
+	printf("%i is zero\n", n);
+}
+else if (n < 0)
+{
+	printf("%i is negative\n", n);
+}
+return (0);
 }
