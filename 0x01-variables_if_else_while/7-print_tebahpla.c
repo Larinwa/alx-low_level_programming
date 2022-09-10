8823370a68e8ed3081d9c567a945f159754aaffa
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-09-09 21:49:05 -0700

    My first commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,13 +2,15 @@
 /**
  * main - entry point
  *
- * Return 0
+ * Return: alway returns 0
+ *
  */
 int main(void)
 {
-char a;
-for (a = 'z'; a >= 'a' ; a--)
-putchar(a);
-putchar('\n');
-return (0);
+        char a;
+
+        for (a = 'z'; a >= 'a' ; a--)
+                putchar(a);
+        putchar('\n');
+        return (0);
 }
