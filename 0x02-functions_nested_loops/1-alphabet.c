7669c599d66eb0fa73defb9cf2f4e7dae428866d
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-09-13 13:40:01 -0700

    first putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * main - print alphabets
+ *
+ * Description: use main function
+ * print alphabet in lowercases
+ * Return 0
+ */
+
+void main(void)
+{
+	int ch;
+	for(ch= 'a'; ch<= 'z'; ch++)
+	putchar(ch);
+	putchar('\n');
+	
+	return (0);
+}
