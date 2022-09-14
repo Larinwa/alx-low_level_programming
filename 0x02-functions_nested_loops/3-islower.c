aada11e7cb2020657f01384e5934125977986228
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-09-14 07:58:39 -0700

    third putpush

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * islower - function to check for lowercases character
+ *
+ * Retutn 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return 1;
+	}
+	else
+		return 0
+}
