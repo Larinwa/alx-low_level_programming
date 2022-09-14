d3cb05c27fc572f73c1527d560a2291d52164b97
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-09-14 12:42:23 -0700

    commit thirteen

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints first 50 Fibonacci numbers suit numbers
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -23,5 +25,5 @@ int main(void)
 			printf(", ");
 	}
 
-	return (0);
-}}
+	return (0)
+}
