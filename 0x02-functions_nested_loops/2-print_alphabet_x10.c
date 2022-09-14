f7f6ae04eb5b28d578cceb1f4e4f1594a9e0cb47
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-09-14 06:13:17 -0700

    second putpush

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function print alphabet ten times in lowercases
+ *
+ * Return 0
+ */
+
+void print_alphabet_x10(void);
+{
+	char c, y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		for (c = 'a'; c = 'z'; y++)
+		{
+			_putchar(c);
+		}
+	}
+}
