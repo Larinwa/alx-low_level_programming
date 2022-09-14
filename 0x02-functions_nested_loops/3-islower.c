a529160c0007464f126b7c30003eaff269b58e47
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-09-14 08:53:46 -0700

    third put

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - function to check for lowercases character
- * c - this int function will be use for the argument
+ * c: this int function will be use for the argument
  * Retutn 0
  */
 int _islower(int c)
