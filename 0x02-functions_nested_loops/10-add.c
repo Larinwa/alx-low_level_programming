d7aaa56de5f0903c97f50d9fc872293c067c8b7b
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-09-14 11:36:39 -0700

    tenth putpush

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,10 @@
-include "main.h"
+#include "main.h"
 
 /**
  * add - Adds two integers.
- * @num1: The first int to be added.
- * @num2: The second int to be added.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ *
  * Return: The result of the addition.
  */
 int add(int num1, int num2)
