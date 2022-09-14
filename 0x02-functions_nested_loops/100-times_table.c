79f4415a8404a9e0711b074cfd55ee696d1c2277
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-09-14 14:58:55 -0700

    My commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,36 +8,38 @@
  * */
 void print_times_table(int n)
 {
-	int digit, mult, result;
+	int num, mult, prod;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (digit = 0; digit <= n; digit++)
+		for (num = 0; num <= n; num++)
 		{
 			_putchar('0');
 
 			for (mult = 1; mult <= n; mult++)
 			{
-					_putchar(',');
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
 					_putchar(' ');
 
-				result = digit * mult;
-			
-				if (result <= 99)
-					_putchar(' ');
-
-				if (result <= 9)
-					_putchar(' ');
-				if (result >= 100)
+				if (prod >= 100)
 				{
-					_putchar((result / 100) + '0');
-					_putchar((result / 10) % 10 + '0');
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
-				else if (result <= 99 && result >= 10)
+				else if (prod <= 99 && prod >= 10)
 				{
-				_putchar((result / 10) + '0');
+				_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
-			_putchar((result % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
