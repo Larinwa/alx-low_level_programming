02da747ebf869abbf8350921ce9ed77057675826
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-09-15 15:22:49 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,86 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /**
-* _putchar - puts character to standard output
-* @c: character to be put to standard output
-*/
+ * File: main.h
+ * Desc: header file
+ */
 
 int _putchar(char c);
-
-/**
- * _isupper - determines uppercase or not
- * @c: character to determine
- * Return: true or false
- */
-
 int _isupper(int c);
-
-/**
- * _isdigit - checks if it is digit
- * @c: character to check
- *
- * Return: returns boolean
- */
-
 int _isdigit(int c);
-
-/**
- * mul - multiplies stuff
- * @a: integer to multiply
- * @b: integer to multiply
- * Return: gives result
- */
-
 int mul(int a, int b);
-
-/**
- * print_numbers - prints the numbers
- *
- */
-
 void print_numbers(void);
-
-/**
- * print_most_numbers - prints the numbers
- *
- */
-
 void print_most_numbers(void);
-
-/**
- * more_numbers - prints the numbers
- *
- */
-
 void more_numbers(void);
-
-/**
- * print_line - prints a
- * @n: integer to print stuff
- */
-
 void print_line(int n);
-
-/**
- * print_diagonal - prints diagonal                *
- * @n: integer to see how many stupid lines
- */
-
 void print_diagonal(int n);
-
-/**
- * print_square - prints a square
- *
- * @size: prints the size of whatever
- */
-
 void print_square(int size);
-
-/**
- * print_triangle - prints triangle
- * @size: size of triangle
- */
-
 void print_triangle(int size);
+void print_number(int n);
 
-#endif
+#endif /* MAIN_H *
