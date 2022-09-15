1434e334a318d6edbfa585f97e184f49da28a8fe
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-09-15 02:46:17 -0700

    My commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,39 +1,44 @@
 #include "main.h"
 
 /**
- * print_times_table - Entry point,
- * @n: number to be treated
- * description: A function that prints the n times
- * table, starting with 0
- * Return: number
+ * print_times_table - Prints the n times table
+ *
+ * @n: number times table (0 < n <= 15)
+ *
+ * Return: no return
  */
-void  print_times_table(int n)
+void print_times_table(int n)
 {
-	int prod, mult, num;
+	int a, b, op;
 
-	if (n <= 15 && n >= 0)
+	if (n >= 0 && n <= 15)
 	{
-	for (num = 0; num <= n; ++num)
-	{
-		_putchar(48);
-		for (mult = 1; mult <= n; ++mult)
+		for (a = 0; a <= n; a++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num * mult;
-			if (prod <= 9)
-				_putchar(' ');
-			if (prod <= 99)
-				_putchar(' ');
-			if (prod >= 100)
+			_putchar(48);
+			for (b = 1; b <= n; b++)
 			{
-				_putchar((prod / 100) + 48);
-				_putchar((prod / 10) % 10 + 48);
-			}
-			else if (prod <= 99 && prod >= 10)
-				_putchar((prod / 10) + 48);
-			_putchar((prod % 10) + 48);
+				op = a * b;
+				_putchar(44);
+				_putchar(32);
+				if (op <=9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(op + 48);
+				}
+				else if (op <= 99)
+				{
+					_putchar(32);
+					_putchar((op / 10) + 48);
+					_putchar((op % 10) + 48);
+				}
+				else
+				{
+					_putchar(((op / 100) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
+					_putchar((op % 10) + 48);
+				}
 			}
 			_putchar('\n');
 		}
