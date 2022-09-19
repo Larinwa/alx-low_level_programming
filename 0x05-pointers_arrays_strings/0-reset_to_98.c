a01922a8f1288830043b33d4dd09b6d97fe96fcc
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-09-19 12:22:04 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
-
-void reset_to_98( )
+/**
+ * void - check the code 
+ *
+ * Return: Always 0.
+ */
+ void reset_to_98(int *n)
 {
-   int *n;
+    *n = 98;
+    
+    return 0;
 
-   *n = 98;
-
-   return 0;
 }
