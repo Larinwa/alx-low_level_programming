f6499404efb267966c9ea81aba48160bdabfeda8
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-09-19 13:41:12 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
+/**
+ * reset_to_98 - function that uses a pointer to
+ * print or updates parameter value
+ * @n: integer pointer
+ * Return: Always 0
+ */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
-
-	return 0;
 
 }
