9d344879dd7d749aace9bc9aa87ac58f3bf68df9
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-10-26 07:49:22 -0700

    list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -29,18 +29,6 @@ typedef struct listp_s
 {
 	void *p;
 	struct listp_s *next;
-
-/**
- * struct listp_s - singly linked list
- * @p: pointers of nodes
- * @next: points to the next node
- *
- * Description: singly linked list of pointers
- */
-typedef struct listp_s
-{
-	void *p;
-	struct listp_s *next;
 } listp_t;
 
 size_t print_listint(const listint_t *h);
