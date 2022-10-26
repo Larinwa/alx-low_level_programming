6cbabe96cc14332bec07926bbe7fb8590d7cdd92
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-10-26 06:50:58 -0700

    lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,37 @@
 #include "lists.h"
 
+
+
 /**
+
  * list_len - A function that gets the number of nodes of linked list
+
  * @h: head of linked list
+
  * Return: number of nodes as size_t
+
  * larinwa = nodes
+
  */
+
 size_t list_len(const list_t *h)
+
 {
+
 	unsigned int larinwa = 0;
 
+
+
 	while (h)
+
 	{
+
 		larinwa++;
+
 		h = h->next;
+
 	}
+
 	return (larinwa);
+
 }
